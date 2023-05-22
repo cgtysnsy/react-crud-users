079a7c408fe26b94cfef562e3c274863b942fd73
cagatay <cgtyeo@gmail.com>
2023-05-22 11:54:58 +0300

    comment: understand the auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import Users from "./components/Users";
 import Footer from "./components/Footer";
-
+//I need some comments on here.
 export default function App() {
   return (
     <div>
